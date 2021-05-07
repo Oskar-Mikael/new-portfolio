259b28db6e04da569dedcf7639b0d9bfce591a41
Oskar-Mikael <oskar.bostrom@chasacademy.se>
2021-05-07 19:07:56 +0200

    Initial commit

---
+++ app/Http/Controllers/IndexController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Project;
+use Illuminate\Http\Request;
+
+class IndexController extends Controller
+{
+    public function index()
+    {
+        $projects = Project::all();
+
+        return view('index', compact('projects'));
+    }
+}
