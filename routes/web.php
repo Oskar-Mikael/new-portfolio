4e76b81c1dba368dfbeca328390298f24896f62a
Oskar-Mikael <oskar.bostrom@chasacademy.se>
2021-05-07 19:08:03 +0200

    IndexController

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AdminController;
+use App\Http\Controllers\IndexController;
 use App\Http\Controllers\PostController;
 use App\Http\Controllers\ProjectController;
 use Illuminate\Support\Facades\Route;
@@ -16,9 +17,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('index');
-});
+Route::get('/', [IndexController::class, 'index']);
+
 Route::get('/contact', function () {
     return view('contact');
 });
